63b099491b9e2756e77a773b3bb7a19a64cbdac4
Manuel Eberle <>
2022-09-09 22:55:40 +0200

    update ZeroDivisionException with author comments

--- src/org.java.examples/ZeroDivisionException.java
+++ src/org.java.examples/ZeroDivisionException.java
@@ -1,3 +1,11 @@
+/**
+ * Thrown if a zero division will happen
+ *
+ * @author  ebeman12
+ * @version 0.1
+ * @date   09.09.2022
+ */
+
 package org.java.examples;
 
 public class ZeroDivisionException extends Exception {
